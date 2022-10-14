aaaa65446b5739cd7260fb34432553dc230c2c04
AutomatizacionesBetcris <115312503+AutomatizacionesBetcris@users.noreply.github.com>
2022-10-13 20:41:38 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Scroll To Top Button
+// BEGIN Scroll go Top Button
 // Get the button:
 mybutton = document.getElementById("myBtn");
 
@@ -6,18 +6,16 @@ mybutton = document.getElementById("myBtn");
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-
-// ------------------------------------------------------
+// END Scroll go Top Button
